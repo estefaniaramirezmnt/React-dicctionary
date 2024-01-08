33d8369ee9eb542de92feefe4063a75b71b959cc
Estefanía <estefaniaramirezmnt@gmail.com>
2024-01-08 11:27:54 +0100

    Added footer and google font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from './components/Footer';
 import './style/App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>Project coded by Nia Ramírez, open-sourced on GitHub</p>
+    </div>
+  );
+};
+
+export default Footer;
