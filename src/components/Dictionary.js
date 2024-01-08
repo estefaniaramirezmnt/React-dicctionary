2f9032af01f2edfcbfc0ad7ab50fb4b616756ad8
Estefanía <estefaniaramirezmnt@gmail.com>
2024-01-08 12:44:40 +0100

    Added basic seach form to dictionary component

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Dictionary.css";
 
 const Dictionary = () => {
+  const [keyWord, setKeyWord] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+    alert(`Searching for "${keyWord}"`);
+  };
+
+  const handleKeyWordChange = (e) => {
+    setKeyWord(e.target.value);
+  }
+
   return (
     <div className="dictionary-container">
       <h1>Dictionary</h1>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeyWordChange}/>
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
 }
