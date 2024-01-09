8cd36ce8574855323704afd3d9c3fa6819d8654c
Estefanía <estefaniaramirezmnt@gmail.com>
2024-01-09 11:32:41 +0100

    Sliced length of synonyms

--- src/components/Synonyms.js
+++ src/components/Synonyms.js
@@ -2,11 +2,12 @@ import React from "react";
 
 const Synonyms = (props) => {
     if (props.synonyms) {
+        const limitedSynonyms = props.synonyms.slice(0, 4);
         return (
         <div className="synonyms-container">
             <strong>Synonyms:</strong>
             <ul>
-                {props.synonyms.map((synonym, index) => {
+                {limitedSynonyms.map((synonym, index) => {
                     return <li key={index}>{synonym}</li>;
                 })}
             </ul>
